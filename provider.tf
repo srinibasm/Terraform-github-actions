3597f1501f115305923652ec70a3610243379a08
Srinibas Mohanty2002 <168587115+srinibasm@users.noreply.github.com>
2026-04-07 22:16:27 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,12 @@
 provider "aws" {
-  region = "ap-south-2"
+  
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = ">4.0" #morthan 4.0 version
+    }
+  }
 }
